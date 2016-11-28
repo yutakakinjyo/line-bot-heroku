091de50a3fca3652e5694c51406318f45ab5575c
yutakakinjyo <yutakakinjyo@gmail.com>
2016-11-28 20:47:31 +0900

    add rack part test

--- test.rb
+++ test.rb
@@ -12,34 +12,34 @@ class LineTest < Test::Unit::TestCase
   end
 
   def test_it_fail_echo
-    # post '/callback', :name => "hoge"
-    # assert_equal last_response.status, 400
+    post '/callback'
+    assert_equal last_response.status, 400
   end
 
   def test_it_echo
-
+    
     ENV["LINE_CHANNEL_SECRET"] = "secret"
-    ENV["LINE_CHANNEL_TOKEN"] = "token"
+
     body = JSON.generate(
            {
              "events": [
-                         {
-                           "replyToken": "replayToken",
-                          "type": "message",
-                          "message": {
-                                       "type": "text",
-                                       "text": "hoge"
-                                     }
-                         }
-                       ]
+               {
+                 "type": "message",
+                 "message": {
+                   "type": "text",
+                   "text": "Hello"
+                 }
+               }
+             ]
            }
          )
-
+    
     hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, ENV["LINE_CHANNEL_SECRET"], body)
     signature = Base64.strict_encode64(hash)
     
     post '/callback', body, {"HTTP_X_LINE_SIGNATURE" => signature}
     assert last_response.ok?
+    assert_equal last_response.body, "OK"
   end
   
 end
